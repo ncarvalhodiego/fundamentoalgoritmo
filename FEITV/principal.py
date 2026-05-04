0e3758eca62ec8e178cb33333e848866dd28f6c8
ncarvalhodiego <diegonc2007@outlook.com>
2026-05-03 23:41:56 -0300

    Add files via upload

--- FEITV/principal.py
+++ FEITV/principal.py
@@ -43,25 +43,57 @@ def formatarUsers():
     with open ("usuarios.txt" , "w") as users:
         users.writelines(novasLinhas)
 
+def adicionarFavs(midiaNome):
+    print("ADICIONOU FAVORITO", midiaNome)
 
 def paginaVideo(video):
     limparTerminal()
-    print("PAGINA VIDEO")
+    print("Titulo: %s" % video[0])
+    print("%s de %s" % (video[5], video[1]))
+    print("Duração: %s" %video[2])
+    print("Lançado em %s" %video[3])
+    print("Tipo de midia: %s" %video[4])
     print()
+    print("1 - Adicionar aos favoritos")
+    print("0 - Voltar")
+    inp = msvcrt.getch().decode()
+    if inp == "1":
+        adicionarFavs(video[0])
+        print
+        return
+    else:
+        menu()
+        return
 
 def paginaFavoritos():
     limparTerminal()
     print("FAVORITOS")
-    with open("favoritos.txt" , "r") as favs:
-        linhas = favs.readlines()
-        for i in linhas:
-            print(i)
 
 def paginaPesquisar():
-    limparTerminal()
-    print("PESQUISA POR NOME:")
-    pesq = str(input(">> "))
-
+    print("PESQUISA POR NOME:  (enter para voltar)")
+    pesq = str(input(">>  "))
+    pesq.strip()
+    if pesq == "":
+        menu()
+        return
+    else:
+        with open("videos.txt", "r") as videos:
+            encontrado = False
+            linhas = videos.readlines()
+            for linha in linhas:
+                linhaS = linha.strip().split("\\")
+                if linhaS[0] == pesq:
+                    linhaST = linhaS
+                    encontrado = True
+            if encontrado:
+                paginaVideo(linhaST)
+                return
+            else:
+                print("Midia não encontrada")
+                sleep(2)
+                limparTerminal()
+                menu()
+                return
 
 def explorar():
     print()
@@ -76,6 +108,7 @@ def explorar():
 
 def menu():
     limparTerminal()
+    global usuario
     print("Bem vindo/a, %s!" % usuario)
     print()
     print("1 - Pesquisar (por nome)")
@@ -85,14 +118,19 @@ def menu():
     print("Explorar:")
     explorar()
     inp = msvcrt.getch().decode()
-    if inp == "0":
-        paginaInicial()
     if inp == "1":
         paginaPesquisar()
-    if inp == "2":
+        return
+    elif inp == "2":
         paginaFavoritos()
+        return
+    elif inp == "0":
+        paginaInicial()
+        usuario = ""
+        return
     else:
         menu()
+        return
 
 def paginaLogin():
     formatarUsers()
@@ -108,11 +146,13 @@ def paginaLogin():
                 sucess = True
     if sucess:
         menu()
+        return
     else:
         print()
         print("Usuário ou senha incorretos!")
         sleep(2)
         paginaInicial()
+        return
 
 def paginaSigin():
     formatarUsers()
@@ -122,6 +162,7 @@ def paginaSigin():
     usuario = usuarior.strip()
     if usuario == "":
         paginaInicial()
+        return
     else:
         jaExiste = False
         with open("usuarios.txt" , "r") as users:
@@ -134,12 +175,14 @@ def paginaSigin():
             sleep(2)
             limparTerminal()
             paginaInicial()
+            return
         else:            
             usersplit = usuario.strip().split()
             if len(usersplit) > 1:
                 print("O nome de usuário não pode conter espaços!")
                 sleep(2)
                 paginaInicial()
+                return
             else:
                 limparTerminal()
                 print("Digite a senha: ")
@@ -151,41 +194,49 @@ def paginaSigin():
                     print("A senha não pode conter espaços!")
                     sleep(2)
                     paginaInicial()
+                    return
                 if len(senhaa) == 0:
                     print("A senha não pode ser nula!")
                     sleep(2)
                     paginaInicial()
+                    return
                 else:
                     if senha1 == senha2:
                         with open("usuarios.txt" , "a") as usuarios:
                             usuarios.write("%s %s\n" % (usuario.strip(), senha1.strip()))
+                        with open("favoritos.txt" , "a") as favs:
+                            favs.write("%s\\\n" %usuario)
                         print("Usuário criado!")
                         sleep(1)
                         print("Você ja pode fazer login!")
                         sleep(2)
                         paginaInicial()
+                        return
                     else:
                         print("As senhas não batem!")
                         sleep(2)
                         paginaInicial()
+                        return
 
 def paginaInicial():
-    limparTerminal()
-    print("Bem Vindo ao FeiTV!")
-    print()
-    print("1 - Login")
-    print("2 - Signin")
-    print("0 - Sair")
-    print()
-    inp = msvcrt.getch().decode()
-    if inp == "1":
-        paginaLogin()
-    elif inp == "2":
-        paginaSigin()
-    elif inp == "0":
+    while True:
         limparTerminal()
-        adeus()
-    else:
-        paginaInicial()
+        print("Bem Vindo ao FeiTV!")
+        print()
+        print("1 - Login")
+        print("2 - Signin")
+        print("0 - Sair")
+        print()
+        inp = msvcrt.getch().decode()
+        if inp == "1":
+            paginaLogin()
+            break
+        elif inp == "2":
+            paginaSigin()
+            break
+        elif inp == "0":
+            limparTerminal()
+            adeus()
+            break
 
 paginaInicial()
